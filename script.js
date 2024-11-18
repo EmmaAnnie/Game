e8c5b9702e61a48a4f6004109011f79150eeeee3
EmmaAnnie <emmabuchta06@gmail.com>
2024-11-18 12:53:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ for (let i = 1; i <= 25; i++) {
     const li = document.createElement('li');
     li.textContent = 'item number $(i)';
     ol.appendChild(li);
-    
+    li.addEventListener("click", () => {
+        if (i % 3 == 0) {
+            console.log("fizz");
+        }
+        console.log('click?');
+    });
 }

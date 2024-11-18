ab1d5d7603bb4d499f1df2976b845156a7dae386
EmmaAnnie <emmabuchta06@gmail.com>
2024-11-18 12:25:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("insane in the membrane");
 for (let i = 1; i <= 25; i++) {
     console.log(i);
     const li = document.createElement('li');
+    li.textContent = i;
     console.log(li);
     
 }

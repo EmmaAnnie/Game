bb404b8d87849b4a86df03fe705ba66c700b490a
EmmaAnnie <emmabuchta06@gmail.com>
2024-11-18 13:17:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ol = document.getElementById("grid")
 for (let i = 1; i <= 25; i++) {
     console.log(i);
     const li = document.createElement('li');
-    li.textContent = 'item number $(i)';
+    li.textContent = i;
     ol.appendChild(li);
     li.addEventListener("click", () => {
         if (i % 3 == 0) {console.log("fizz"); }
